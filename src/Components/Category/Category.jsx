05c517250bb51e3789df66fefa438012495c834f
Shimul Das <shimul.191002110@gmail.com>
2023-04-10 00:07:36 +0600

    category list perfectly work

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import
+import './category.css'
 import CategoryDetails from '../CategoryDetails/CategoryDetails';
 
 const Category = () => {
@@ -10,10 +10,15 @@ const Category = () => {
         .then(data=>setcategories(data))
         },[]);
   return (
+    <div>
+        <h1 className='c-title'>Job Category List</h1>
+        <p className='c-info'>Explore thousands of job opportunities with all the information you need. Its your future</p>
     <div className='category'>
+        
 {
     categories.map(categorie=><CategoryDetails key={categorie.id} categorie={categorie}></CategoryDetails>)
 }
+    </div>
     </div>
   )
 }
--- src/Components/CategoryDetails/CategoryDetails.jsx
+++ src/Components/CategoryDetails/CategoryDetails.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import './categorydetail.css'
 const CategoryDetails = ({categorie}) => {
     console.log(categorie);
     const {id,category_logo,category_name,job_available}=categorie;
   return (
-    <div >
+    <div className='category-detail' >
         <img src={category_logo} alt="" />
-        <h2>{category_name}</h2>
-        <p>{job_available}<span> Jobs Available</span></p>
+        <h2 className='category-name'>{category_name}</h2>
+        <p className='job-available'>{job_available}<span> Jobs Available</span></p>
     </div>
   )
 }
