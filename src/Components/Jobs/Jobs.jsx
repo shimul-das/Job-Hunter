e8e519cf31897f028c6b26924e2dd47405106b99
Shimul Das <shimul.191002110@gmail.com>
2023-04-11 00:56:49 +0600

    see all button done

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -1,22 +1,56 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import Job from '../Job/Job';
+
+// const Jobs = () => {
+//   const [jobs,setjobs]=useState([]);
+//   useEffect(()=>{
+//       fetch('jobs.json')
+//       .then(res=>res.json())
+//       .then(data=>setjobs(data))
+//       },[]);
+//       console.log(jobs)
+// return (
+// <div>
+//   {
+// jobs.map(job=><Job key={job.id} job={job}></Job>)
+//   }
+//   <button>see all</button>
+// </div>
+// )
+// }
+
+// export default Jobs
+////////////////////////////
+import React, { useEffect, useState } from 'react';
 import Job from '../Job/Job';
 
 const Jobs = () => {
-  const [jobs,setjobs]=useState([]);
-  useEffect(()=>{
-      fetch('jobs.json')
-      .then(res=>res.json())
-      .then(data=>setjobs(data))
-      },[]);
-      console.log(jobs)
-return (
-<div>
-  {
-jobs.map(job=><Job key={job.id} job={job}></Job>)
-  }
-  
-</div>
-)
-}
+  const [jobs, setJobs] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+  const [limit, setLimit] = useState(4); // add a state variable for the limit
 
-export default Jobs
+  useEffect(() => {
+    fetch('jobs.json')
+      .then(res => res.json())
+      .then(data => setJobs(data));
+  }, []);
+
+  const handleSeeAll = () => {
+    setShowAll(true);
+    setLimit(jobs.length); // update the limit to the length of jobs array
+  };
+
+  return (
+    <div>
+      {jobs.slice(0, limit).map(job => ( // use slice with the limit
+        <Job key={job.id} job={job} />
+      ))}
+      {!showAll && (
+        <button onClick={handleSeeAll}>See all</button>
+      )}
+    </div>
+  );
+};
+
+export default Jobs;
+
