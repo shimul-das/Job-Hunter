096c9eb51de0a610faccc2729d55da9fab76ab17
Shimul Das <shimul.191002110@gmail.com>
2023-04-11 14:10:31 +0600

    bog page 4 questions are added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -6,41 +6,40 @@ return (
 <div className='blogs_1'>
     <h2 className='blog-title_1'>Blogs</h2>
     <div className='questions'>
-        <h3>1. What is the difference between Props vs state? </h3>
-        <p><span className='span'>Props (short for "properties") </span>are used to pass data from a parent component to
-            a child component. They are immutable and read-only, meaning that they cannot be modified by the child
-            component that receives them. Instead, the child component can use the props passed down to it to render its
-            own UI or pass them on to other child components.</p>
-        <p><span className='span'>State</span> on the other hand, is used to manage data that changes within a
-            component. Unlike props, state is mutable and can be changed by the component itself through a method called
-            setState(). When the state changes, React will automatically re-render the component to reflect the updated
-            data.</p>
+        <h3>1. When should you use context API? </h3>
+        <p><span className='span'>Context API </span>is a feature of React that allows
+            you to pass data through your component trees without using props. It is useful when you need to share state
+            or functions between different components at different levels of the hierarchy. It can also help you avoid
+            prop drilling, which is when you pass props through many components that don’t need them. Context API
+            consists of three steps: defining a context object, providing a context value in a common ancestor
+            component, and consuming the context value in any child component. You should use Context API when your
+            state needs to be accessed or set from deeply nested or many child components</p>
     </div>
     <div className='questions'>
-        <h3>2. How does useState work?</h3>
-        <p><span className='span'>useState</span> is a React Hook that lets you add a state variable to your component.
-            It returns an array with two values: the current state and a function to update it. You can use the function
-            to change the state value and trigger a re-render of your component. You can also pass a function as an
-            argument to useState to initialize the state based on some calculation.</p>
+        <h3>2. What is a custom hook?</h3>
+        <p><span className='span'>A custom hook</span> is a function that starts with “use” and may call other hooks.
+            Custom hooks allow you to reuse component logic that needs to be shared by multiple components. Custom hooks
+            follow the same rules as built-in hooks, such as only calling them at the top level and only from React
+            function components or other custom hooks. Custom hooks can return any value that is useful for the
+            components that use them</p>
 
     </div>
     <div className='questions'>
-        <h3>3. What is the Purpose of useEffect other than fetching data.</h3>
-        <p><span className='span'>useState</span> is a React Hook that lets you perform side effects in your components.
-            Here are some other examples of can do with useEffect besides fetching data:DOM manipulation, Set up
-            subscriptions , Manage timers and animations , Call APIs or services and Handle browser events. useEffect
-            accepts a function and an optional array of dependencies as arguments. The function will run after every
-            render unless you specify the dependencies that control when it should run. You can also return a cleanup
-            function from useEffect to dispose any resources that are no longer needed</p>
+        <h3>3.What is useRef?</h3>
+        <p><span className='span'>useRef is a</span> React hook that lets you create a reference to a value
+            that persists between renders. It returns an object with a current property that can be accessed and
+            mutated. useRef does not cause a re-render when the current property is changed. It can be used to store a
+            mutable value that does not affect the rendering, to access a DOM element directly, or to keep track of
+            previous state values</p>
 
     </div>
     <div className='questions'>
-        <h3>4. How Does React work? </h3>
-        <p><span className='span'>React</span> is a JavaScript library that creates user interfaces using declarative
-            code and reusable components. React components are like JavaScript functions that accept input data (props)
-            and return React elements that describe how the UI should look. React uses a virtual DOM to efficiently
-            update the UI based on changes in the data (state). React can also be used to build single-page applications
-            and mobile apps with other libraries</p>
+        <h3>4. What is useMemo? </h3>
+        <p><span className='span'>useMemo is a</span>  React hook that lets you cache the result of a calculation
+            between re-renders. It accepts a function and a list of dependencies as arguments. It returns the result of
+            calling the function on the initial render and on subsequent renders if any of the dependencies have
+            changed. Otherwise, it returns the cached value from the previous render. useMemo can improve performance by
+            avoiding expensive recalculations or re-rendering of components</p>
     </div>
 </div>
 )
